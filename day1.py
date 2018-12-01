aa249e195a29a1ce4e3e806b7ca395896aad4048
Grant Williams <grant.williams@okstate.edu>
2018-12-01 14:39:38 -0600

    upload day1

---
+++ day1.py
@@ -0,0 +1,9 @@
+# Part 1
+from functools import reduce
+print(reduce(lambda x,y: eval(str(x)+y), open('data.txt').read().splitlines()))
+
+# part 2
+from itertools import cycle, accumulate
+
+s = set([0]) 
+print(next(res for res in accumulate(cycle(map(int,open('data.txt').read().splitlines()))) if res in s or s.add(res))
